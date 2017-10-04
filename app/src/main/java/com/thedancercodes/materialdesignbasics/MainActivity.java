8aaaa5bee9806b36abdf23a3a64b703a85a987ce
Roger Taracha <rojtaracha@gmail.com>
2017-10-04 12:34:01 +0300

    Add Menu to App: [+] menu_main file, menu drawable and implementation in MainActivity class.

--- app/src/main/java/com/thedancercodes/materialdesignbasics/MainActivity.java
+++ app/src/main/java/com/thedancercodes/materialdesignbasics/MainActivity.java
@@ -19,4 +19,27 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
     }
+
+        @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        // Inflate the menu; This adds items to the AppBar if it is present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        // Handle AppBar item clicks here.
+        // The AppBar will automatically handle clicks on the Home/Up button, so long as you
+        // specify a parent activity in AndroidManifest.xml
+        int id = menuItem.getItemId();
+
+        // noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(this, "Hey you just hit " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(menuItem);
+    }
 }
