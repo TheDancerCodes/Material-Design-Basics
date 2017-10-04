ec87cb674e12648d31b9ed600056193cfa8aa658
Roger Taracha <rojtaracha@gmail.com>
2017-10-04 11:50:40 +0300

    Add a Toolbar to the App: [+] Define the app_bar.xml containing a toolbar. [+] Use <include> in your layout.xml file to add to the ToolBar. [+] Instantiate the ToolBar using findViewById inside the Activity. [+] Call setSupportActionBar() and pass your ToolBar object inside.

--- app/src/main/java/com/thedancercodes/materialdesignbasics/MainActivity.java
+++ app/src/main/java/com/thedancercodes/materialdesignbasics/MainActivity.java
@@ -2,12 +2,21 @@ package com.thedancercodes.materialdesignbasics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 }
